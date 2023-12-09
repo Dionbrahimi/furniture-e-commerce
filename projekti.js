e4c3f9c961a05af2b0371e2df314188209b46e94
diongjoshii <dg64918@ubt-uni.net>
2023-12-09 21:36:38 +0100

    finished responsive design and some little css changes

--- projekti.js
+++ projekti.js
@@ -1,19 +1,47 @@
 function validateForm() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var errorMessage = document.getElementById('error-message');
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let errorMessage = document.getElementById("error-message");
 
-    var emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailValid.test(email)) {
-        errorMessage.innerHTML = 'Please enter a valid email address.';
-        return false;
-    }
+  let emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (password.length < 8 || !/[0-9]/.test(password)) {
-        errorMessage.innerHTML = 'Password must be at least 8 characters long and contain at least one number.';
-        return false;
-    }
-
-    window.location.href = 'home-page.html';
+  if (!emailValid.test(email)) {
+    errorMessage.innerHTML = "Please enter a valid email address.";
+    return false;
+  } else if (password.length < 8 || !/[0-9]/.test(password)) {
+    errorMessage.innerHTML =
+      "Password must be at least 8 characters long and contain at least one number.";
+    return false;
+  } else {
+    window.location.href = "home-page.html";
     return false;
   }
+}
+
+const hamburgerIcon = document.querySelector(".uil-bars");
+const responsiveNav = document.getElementById("responsiveNav");
+
+const closeIcon = document.querySelector('.close-icon');
+
+const navbarLinks = document.querySelectorAll('.responsive-nav__list a');
+
+
+hamburgerIcon.addEventListener("click", () => {
+    if (responsiveNav.style.display === "block") {
+      responsiveNav.style.display = "none";
+    } else {
+      responsiveNav.style.display = "block";
+    }
+  });
+
+closeIcon.addEventListener('click', () => {
+  responsiveNav.style.display = 'none';
+});
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    responsiveNav.style.display = 'none';
+  });
+});
+
+
