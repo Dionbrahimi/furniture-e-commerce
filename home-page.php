1c57facf456348567dbebb6f92e810267f905b16
diongjoshii <dg64918@ubt-uni.net>
2024-01-24 22:21:58 +0100

    added slider in home page

--- home-page.php
+++ home-page.php
@@ -10,6 +10,10 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
     rel="stylesheet" />
 
+    <!-- OWL CAROUSEL LINKS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"> 
 
   <title>Furniture Website</title>
@@ -62,6 +66,25 @@
       </div>
     </div>
   </header>
+
+
+  <section style="margin-top: 80px;">
+
+
+  <div class="owl-carousel owl-theme">
+    <div style="width: 400px; margin: auto" class="item">          
+      <img src="./assets/images/header-bg.png" alt="Logo" />
+</div>
+    <div style="width: 400px; margin: auto" class="item">
+    <img src="./assets/images/visualize-ideas1.png" alt="visualize-ideas">
+    </div>
+    <div style="width: 400px; margin: auto" class="item">
+    <img src="./assets/images/visualize-ideas3.png" alt="visualize-ideas">
+    </div>
+  
+</div>
+
+</section>
 
   <main>
     <section class="why-choosing-us container">
@@ -221,7 +244,29 @@
   </footer>
 </body>
 
+    <!-- OWL CAROUSEL SCRIPTS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script src="projekti.js"></script>
+
+<script>
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+</script>
 
 </html>
